5f7e38ae032c632b8078d5e114f2f31df2f09225
Titus Wormer <tituswormer@gmail.com>
2019-05-12 11:01:32 +0200

    Remove superfluous comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict'
 
-/* Expose. */
 module.exports = hidden
 
-/* Check if `filename` is hidden (starts with a dot). */
 function hidden(filename) {
   if (typeof filename !== 'string') {
     throw new Error('Expected string')
