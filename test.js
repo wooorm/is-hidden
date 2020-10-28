061bbde07f4adf2b5fdf6c60acd346efb6740f56
Titus Wormer <tituswormer@gmail.com>
2020-10-28 11:01:27 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,14 +3,14 @@
 var test = require('tape')
 var hidden = require('.')
 
-test('hidden(filename)', function(t) {
+test('hidden(filename)', function (t) {
   t.equal(hidden('.git'), true, 'should work (1)')
   t.equal(hidden('git'), false, 'should work (2)')
   t.equal(hidden('.DS_Store'), true, 'should work (3)')
   t.equal(hidden('DS.Store'), false, 'should work (4)')
 
   t.throws(
-    function() {
+    function () {
       hidden(true)
     },
     /^Error: Expected string$/,
